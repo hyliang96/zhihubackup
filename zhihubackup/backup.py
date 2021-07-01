a0ede5669bdc187454e835ccf3cd7069fb58d7f5
L-M-Sherlock <jarrett.ye@outlook.com>
2021-07-02 01:43:32 +0800

    转义title中的分隔符

--- zhihubackup/backup.py
+++ zhihubackup/backup.py
@@ -64,7 +64,7 @@ def loop(username):
                 if 'api' in target['url']:
                     target['url'] = target['url'].replace('api', 'www').replace('answers', 'answer')
                 with open(os.path.join("index.csv"), 'a', encoding='utf-8') as f:
-                    f.write(','.join([title, target['url'], target_type, '\n']))
+                    f.write(','.join([title.replace(',', '，'), target['url'], target_type, '\n']))
             title = '-' + validate_title(title) if title != '' else ''
             with open(os.path.join(username, target_type, "%s%s.html" % (target['id'], title)), 'w', encoding='utf-8') as f:
                 f.write('\n'.join(saved))
